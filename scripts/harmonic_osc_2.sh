8fed366e7edde961ab91820d363ae476e8be0c5f
moritzhauschulz <moritz.hauschulz@gmail.com>
2025-04-27 19:00:54 +0100

    anonymous test

--- scripts/harmonic_osc_2.sh
+++ scripts/harmonic_osc_2.sh
@@ -7,22 +7,22 @@ SCRIPT_NAME=$(basename "$0" .sh)
 
 counter=4
 
-# for strat in vanilla QR normal; do
-#     for i in {1..5}; do
-#         python main.py --problem harmonic_oscillator --device cpu --method deeponet \
-#                 --branch_layers 3 128 128 128 8 --trunk_layers 1 128 128 128 4 \
-#                 --tmax 10 \
-#                 --epochs 500 --load_data False --loss mse \
-#                 --strategy $strat \
-#                 --branch_weight 0 --trunk_weight 0 \
-#                 --num_outputs 2 --experiment_name "${SCRIPT_NAME}_${counter}" \
-#                 --IC '{"q0": [-1,1], "p0": [-1,1], "omega": [2,2]}'
-#     done
-#     ((counter++))   
-# done
+for strat in vanilla QR normal; do
+    for i in {1}; do
+        python main.py --problem harmonic_oscillator --device cpu --method deeponet \
+                --branch_layers 3 128 128 128 8 --trunk_layers 1 128 128 128 4 \
+                --tmax 10 \
+                --epochs 500 --load_data False --loss mse \
+                --strategy $strat \
+                --branch_weight 0 --trunk_weight 0 \
+                --num_outputs 2 --experiment_name "${SCRIPT_NAME}_${counter}" \
+                --IC '{"q0": [-1,1], "p0": [-1,1], "omega": [1,1]}'
+    done
+    ((counter++))   
+done
 
 for strat in normal; do
-    for i in {1..5}; do
+    for i in {1}; do
         python main.py --problem harmonic_oscillator --device cpu --method deeponet \
                 --branch_layers 3 128 128 128 8 --trunk_layers 1 128 128 128 4 \
                 --tmax 10 \
@@ -31,7 +31,7 @@ for strat in normal; do
                 --strategy $strat \
                 --branch_weight 0 --trunk_weight 0 \
                 --num_outputs 2 --experiment_name "${SCRIPT_NAME}_${counter}" \
-                --IC '{"q0": [-1,1], "p0": [-1,1], "omega": [2,2]}'
+                --IC '{"q0": [-1,1], "p0": [-1,1], "omega": [1,1]}'
         done
     ((counter++))   
 done
